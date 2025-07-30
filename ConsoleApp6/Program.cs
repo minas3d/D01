989fa046d3123d914c33f66d95710608694caf62
ELASAD <ELASAD@DESKTOP-5CVCH0O>
2025-07-30 20:10:20 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(" Enter the number of large carpets ");
+int large = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Enter the number of small carpets  ");
+int small = Convert.ToInt32(Console.ReadLine());
+double sum = (large * 35 )+ (small * 25);
+double tax = sum / 100;
+double total = tax * 6 + sum;
+Console.WriteLine( total);
